3cec2259aeb09a2c7730eff316ff077b37571140
Yongjian Bai <byj6696@gmail.com>
2023-08-11 14:42:01 +0800

    configures endpoint authorization with the same behavior as the default one.

--- src/main/java/com/byodian/security/config/ProjectConfig.java
+++ src/main/java/com/byodian/security/config/ProjectConfig.java
@@ -2,11 +2,14 @@ package com.byodian.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -22,5 +25,16 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // configures endpoint authorization with the same behavior as the default one.
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(
+            authz -> authz.anyRequest().authenticated()
+        );
+
+        return http.build();
     }
 }
