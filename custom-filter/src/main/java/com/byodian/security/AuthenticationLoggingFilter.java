75272bad20b588786eec8622cffdadfe2bc48a2f
byodian <byj6696@gmail.com>
2023-09-17 22:21:37 +0800

    set up filterchain

---
+++ custom-filter/src/main/java/com/byodian/security/AuthenticationLoggingFilter.java
@@ -0,0 +1,22 @@
+package com.byodian.security;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class AuthenticationLoggingFilter implements Filter {
+    @Override
+    public void doFilter(
+        ServletRequest request,
+        ServletResponse response,
+        FilterChain chain
+    ) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) request;
+
+        var requestId = httpRequest.getHeader("Request-Id");
+        System.out.println("Successfully authenticated request with id: " + requestId);
+        chain.doFilter(request, response);
+    }
+}
---
+++ custom-filter/src/main/java/com/byodian/security/config/ProjectConfig.java
@@ -0,0 +1,47 @@
+package com.byodian.security.config;
+
+import com.byodian.security.AuthenticationLoggingFilter;
+import com.byodian.security.RequestValidationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    UserDetailsService userDetailsService() {
+        var user = User.withUsername("john")
+            .password("12345")
+            .authorities("read")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    // To customise authentication and authorization
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(// Adding a filter before an existing one in the chain
+            new RequestValidationFilter(),
+            BasicAuthenticationFilter.class
+        )
+        .addFilterAfter( // Adding a filter after an existing one in the chain
+            new AuthenticationLoggingFilter(),
+            BasicAuthenticationFilter.class
+        ).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
