8d704d7f0672cefc2ae8337e4847bc760beccba1
Yongjian Bai <byj6696@gmail.com>
2023-10-11 16:12:00 +0800

    OncePerRequstFilter

--- custom-filter/src/main/java/com/byodian/security/AuthenticationLoggingFilter.java
+++ custom-filter/src/main/java/com/byodian/security/AuthenticationLoggingFilter.java
@@ -3,25 +3,23 @@ package com.byodian.security;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @Component
-public class AuthenticationLoggingFilter implements Filter {
-    private final Logger logger = LoggerFactory.getLogger(AuthenticationLoggingFilter.class);
+public class AuthenticationLoggingFilter extends OncePerRequestFilter {
+    private final Logger logger = Logger.getLogger(AuthenticationLoggingFilter.class.getName());
 
     @Override
-    public void doFilter(
-        ServletRequest request,
-        ServletResponse response,
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
         FilterChain chain
     ) throws IOException, ServletException {
-        var httpRequest = (HttpServletRequest) request;
-
-        var requestId = httpRequest.getHeader("Request-Id");
+        var requestId = request.getHeader("Request-Id");
         logger.info("Successfully authenticated request with id: " + requestId);
         chain.doFilter(request, response);
     }
